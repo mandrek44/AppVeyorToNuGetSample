50bf06e2f0fa4082b4a73c22a4007f15c66150ba
Marcin Drobik <mdrobik@future-processing.com>
2014-07-13 12:36:37 +0200

    Initial commit

---
+++ AppVeyorToNuGetSample/Foo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace AppVeyorToNuGetSample
+{
+    public static class Foo
+    {
+        public static IEnumerable<string> Bar(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    yield return "FooBar";
+                }
+                if (i % 3 == 0)
+                {
+                    yield return "Foo";
+                }
+                else if (i % 5 == 0)
+                {
+                    yield return "Bar";
+                }
+                else
+                {
+                    yield return i.ToString();
+                }
+            }
+        }
+    }
+}
